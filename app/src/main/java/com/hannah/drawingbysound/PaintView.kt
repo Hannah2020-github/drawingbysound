7cb50328542faab12223504c300602699648ff73
Hannah2020-github <devonclass712@gmail.com>
2024-07-30 16:06:22 +0800

    feature: layout 插入自定義 view

---
+++ app/src/main/java/com/hannah/drawingbysound/PaintView.kt
@@ -0,0 +1,8 @@
+package com.hannah.drawingbysound
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PaintView(c: Context, attrs: AttributeSet): View(c, attrs) {
+}
